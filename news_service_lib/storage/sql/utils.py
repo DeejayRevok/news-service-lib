b3f082cffaaff123f485fccff647f11dfa99f073
DeejayRevok <seryi_one@hotmail.com>
2020-12-30 21:45:37 +0100

    Add alembic initialization logic to init_db sql util

--- news_service_lib/storage/sql/utils.py
+++ news_service_lib/storage/sql/utils.py
@@ -1,23 +1,36 @@
 """
 SQL utilities module
 """
+from alembic import command
+from alembic.config import Config
+from sqlalchemy import inspect
 from sqlalchemy.engine import Engine, create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from .engine_type import SqlEngineType
 
 
-def init_sql_db(base: DeclarativeMeta, engine: Engine):
+def init_sql_db(base: DeclarativeMeta, engine: Engine, alembic_ini_path: str = None):
     """
-    Initialize the sql database schema defined by the base input schema
+    Initialize the sql database schema defined by the base input schema running the migrations if it is needed
 
     Args:
         base: database base schema
         engine: database engine used to create the tables
+        alembic_ini_path: alembic tool initialization file
 
     """
     base.metadata.bind = engine
-    base.metadata.create_all()
+    if alembic_ini_path:
+        alembic_cfg = Config(alembic_ini_path)
+        alembic_cfg.set_section_option('alembic', 'sqlalchemy.url', str(engine.url))
+        if len(inspect(engine).get_table_names()):
+            command.upgrade(alembic_cfg, "head")
+        else:
+            base.metadata.create_all()
+            command.stamp(alembic_cfg, "head")
+    else:
+        base.metadata.create_all()
 
 
 def create_sql_engine(engine_type: SqlEngineType, **engine_config) -> Engine:
